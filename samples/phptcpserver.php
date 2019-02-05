a4b4495cd344d301e05b7427762e202b1d0a38a4
baiguiren <916809498@qq.com>
2019-02-05 17:06:44 +0800

    socket_set_nonblock

--- samples/phptcpserver.php
+++ samples/phptcpserver.php
@@ -100,7 +100,7 @@ while (true) {
         if ($write == $local_sock && $to_local != '') {
             echo "write to local -----------\n";
             socket_write($write, $to_local);
-            $to_local = '';
+//            $to_local = '';
             echo "write to local ----------- end\n";
         }
         if ($write == $external_sock && $to_external != '') {
