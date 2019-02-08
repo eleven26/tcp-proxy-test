a7b5784daaea6e6ec04d095659857709a0e2b551
baiguiren <916809498@qq.com>
2019-02-08 12:56:41 +0800

    debug

--- src/ProxyServer.php
+++ src/ProxyServer.php
@@ -123,7 +123,9 @@ class ProxyServer
                 // 读取到数据的几种情况
                 // 1. 外网请求，需要转发到内网
                 // 2. 内网返回，需要返回给外网
+                echo "before read ...\n";
                 $data = socket_read($read, $this->bytesLength + $this->identityLength);
+                echo "after read ...\n";
 
                 if ($data !== '') {
                     echo "receive data from: $ip:$port" . PHP_EOL;
